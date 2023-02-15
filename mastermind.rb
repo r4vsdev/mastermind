0bb738303fb2736a8723c978cd94fd1f43558d2a
r4vsdev <r4vsdev@gmail.com>
2023-02-15 02:02:38 -0300

    Add minor fixes

--- mastermind.rb
+++ mastermind.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 class Game
-  attr_reader :secret, :game_over, :attempts_left
+  attr_reader :game_over, :attempts_left
 
   def initialize
     @game_over = false
-    @attempts_left = 3
+    @attempts_left = 12
     @avaiable_numbers = [1, 2, 3, 4, 5, 6, 7, 8]
     @secret = @avaiable_numbers.shuffle.take(4)
   end
 
-  def get_player_move
+  def player_move
     @player_move = gets.chomp.split('').map(&:to_i).uniq
   end
 
@@ -17,23 +19,19 @@ class Game
     @player_move.each do |guess|
       if @secret.include?(guess)
         i = @secret.index(guess)
-        if @secret[i] == @player_move[i]
-          feedback_arr << 'X'
-        else
-          feedback_arr << 'O'
-        end
+        feedback_arr << (@secret[i] == @player_move[i] ? 'X' : 'O')
       end
     end
-    return feedback_arr
+    feedback_arr
   end
 
-  def is_over?
-    if self.feedback == ['X', 'X', 'X', 'X']
+  def over?
+    if feedback == %w[X X X X]
       @game_over = true
-      puts "Congratulations! You Won"
-    elsif @attempts_left == 0
+      puts 'Congratulations! You Won'
+    elsif @attempts_left.zero?
       puts 'You Lost!'
-      return @game_over = true
+      @game_over = true
     else
       @game_over = false
     end
@@ -46,15 +44,13 @@ end
 
 game = Game.new
 
-until game.game_over do
+until game.game_over
   puts 'Guess the correct sequence by choosing 4 numbers in the correct order'
   puts "You have #{game.attempts_left} attempts left"
-  player_move = game.get_player_move
+  game.player_move
   puts
-  puts "player_move = #{player_move}"
-  puts "secret      = #{game.secret}" # apagar :secret la em cima junto
   puts "feedback    = #{game.feedback}"
   game.remove_attempt
-  game.is_over?
+  game.over?
   puts
 end
