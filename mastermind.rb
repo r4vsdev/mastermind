87080f15eb5900fab4fe7e8f81f8e3688acf2dfa
r4vsdev <r4vsdev@gmail.com>
2023-02-15 00:43:13 -0300

    Add winning condition

--- mastermind.rb
+++ mastermind.rb
@@ -1,20 +1,14 @@
-require 'pry-byebug'
-
-# computer sets the secret
-# player choose the try
-# send feedback to player
-#   if color and sequence are right:
-#     feedback_arr << 'X'
-#   if color is right:
-#     feedback_arr << 'O'
-
 class Game
-  attr_reader :secret, :feedback_arr
+  attr_reader :secret, :feedback_arr, :game_over
+
   def initialize
     @game_over = false
     @avaiable_numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-    # @secret = @avaiable_numbers.shuffle.take(4)
-    @secret = [1,2,3,6]
+    @secret = @avaiable_numbers.shuffle.take(4)
+  end
+
+  def get_player_move
+    @player_move = gets.chomp.split('').map(&:to_i).uniq
   end
   
   def feedback
@@ -26,26 +20,29 @@ class Game
           feedback_arr << 'X'
         else
           feedback_arr << 'O'
-        end     
+        end 
       end
     end
     return feedback_arr
   end
-  
-  def get_player_move
-    # @player_move = gets.chomp.split('').map(&:to_i)
-    @player_move = [9,1,3,4]
-  end
 
+  def is_over?
+    if self.feedback == ['X', 'X', 'X', 'X']
+      @game_over = true
+      puts "Congratulations! You Won"
+    else
+      @game_over = false
+    end
+  end
 end
 
 game = Game.new
 
-puts 'Guess the correct sequence by choosing 4 numbers in the correct order'
-until @game_over do 
+until game.game_over do
+  puts 'Guess the correct sequence by choosing 4 numbers in the correct order'
   player_move = game.get_player_move
   puts "player_move = #{player_move}"
   puts "secret      = #{game.secret}"
   puts "feedback    = #{game.feedback}"
-  break
+  game.is_over?
 end
